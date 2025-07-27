32580980f25d1fa793074b81454b70de903be4c6
Anup-sharma190 <anupcg06433@gmail.com>
2025-07-27 23:18:11 +0530

    all done perfect

---
+++ string extraction.py
@@ -0,0 +1,16 @@
+str= "rahul shetty"
+str2=("great teacher")
+print(str+" "+str2)
+str3 = "rahul"
+if str3 in str:
+    print("true")
+else:
+    print("false")
+print(str3 in str)
+string= "rahulshetty"
+print(string[0:5])
+x= "anup.com"
+var= x.split(".")
+print(var)
+print(type(var))
+print(var[0])
---
+++ test string function project.py
@@ -0,0 +1,24 @@
+str= "rahul shetty"
+str2=("great teacher")
+print(str+" "+str2)# string concatenation
+str3 = "rahul"
+if str3 in str:
+    print("true")
+else:
+    print("false")
+print(str3 in str)# check string is present in parent string or not to do testing of string
+string= "rahulshetty"
+print(string[0:5])
+x= "anup.com"
+var= x.split(".")# split
+print(var)
+print(type(var))
+print(var[0])# extract value useing index
+a= (" rahul ")
+print(a.strip()) # remove space both end
+a= (" rahul ")
+print(a.lstrip())# to remove space from left
+a= (" rahul ")
+print(a.rstrip())# to remove space from right
+a= (" rahul ")
+print(a.strip())
