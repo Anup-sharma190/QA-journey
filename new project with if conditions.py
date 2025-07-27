3074c992e5b4b8e997769d759b1aac761408ade6
Anup-sharma190 <anupcg06433@gmail.com>
2025-07-27 11:23:16 +0530

    all done

---
+++ new project with if conditions.py
@@ -0,0 +1,52 @@
+
+greeting="good morning"
+if greeting == "good morning":
+    print("condition match")
+else:
+    print("condition not match")
+print("if else is over")
+a= 5
+if a>6:
+    print("good")
+else:
+    print("koi na")
+print("chalega")
+A=[1,2,3,4,5,6]
+for i in A:
+    print(i*2)
+# SUM OF NATURAL NUMABER (1+2+3+4+5) =15
+SUM = 0
+for j in range (1,6):
+    SUM = SUM +j
+print(SUM)
+for k in range(10):
+    print(k)
+IT = 8
+while IT> 2:
+    if IT!= 5:
+        print(IT)
+    IT= IT-1
+print("done")
+IT = 20
+while IT> 2:
+    if IT == 5:
+        break
+        print(IT)
+    IT = IT - 1
+print("done")
+it= 10
+while it>1:
+    if it==5:
+        it= it-1
+        continue
+print("done")
+while it>1:
+    if it==9:
+        it=it-1
+        continue
+    if it == 3:
+        break
+    print(it)
+
+
+
---
+++ project simple.py
@@ -0,0 +1,73 @@
+print("hello")
+print("anup")
+a = 3
+print(a)
+str = "hello world"
+print(str)
+b, c, d = 5,6.4,"goa"
+print("{} {}".format ("value is", b))
+print(type(b))
+
+"A"== "6"
+"B"== "7"
+print("A" +" "+ "B")
+VAL = [1, 2, 2.3, "RAHUL"]
+print(VAL[1])
+print(type(VAL))
+VAL[2]= "rahul"
+print(VAL)
+A= {1:"FIRST NAME" , 2:"LAST NAME"}
+print(type(A))
+print(A[1])
+print(A[2])
+DICT={}
+A={}
+A["NAME"] = "ANUP"
+A["SURNAME"] = "SHARMA"
+A["SISTER"] = "MINI"
+A["AGE"]= 35
+print(A)
+print(type(A))
+print(A["SURNAME"])
+fruits= ["apple", "banana", "cherry", "date", "elderberry"]
+print("First fruit:", fruits[0])
+print("Last fruit:", fruits[-1])
+print(fruits[1:3])
+person= ("Rahul", 25, 5.9)
+print("Age:", person[1])
+car={"make": "Toyota","model": "Camry","year": 2020,"color": "Blue"}
+print("Car model:", car[1])
+car={"make": "Toyota", "model": "Camry", "year": 2020, "color": "Blue", "owner": "Rahul"}
+print("Car model:", car[1])
+car={"make": "Toyota", "model": "Camry", "year": 2020, "color": "Blue"}
+print("Car model:", car["model"])
+car={"make": "Toyota", "model": "Camry", "year": 2020, "color": "Blue"}
+print("Car model:", car["model"])
+car["owner"]=  "Rahul"
+print(car)
+car = {"make": "Toyota", "model": "Camry", "year": 2020, "color": "Blue" }
+print("Car model:", car["model"])
+car["owner"] = "Rahul"
+print(car)
+car = {"make": "Toyota", "model": "Camry", "year": 2020, "color": "Blue"}
+print("Car model:", car["model"])
+car["owner"] = "Rahul"
+print(car)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
