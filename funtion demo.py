0889e6dc1f013bf154295a25b3ff6a96961c056d
Anup-sharma190 <anupcg06433@gmail.com>
2025-07-27 21:10:35 +0530

    all done

---
+++ funtion demo.py
@@ -0,0 +1,14 @@
+def greetme():
+    print("good morning")
+
+greetme()
+def greetme(name):
+    print("good morning"+ " "+ name)
+greetme("rahul shetty")
+print(8888888888)
+
+
+def addvariable (a,b):
+    return a+b
+print(addvariable (5,5))
+
---
+++ list.py
@@ -0,0 +1,12 @@
+values = [1, 2, "rahul", 1.0, 25]
+print(values[0])
+print(values[-1])
+print(values[0:-1])
+values.insert(3, "biru")
+print(values)
+values.append("end")
+print(values)
+values[2]= "RAHUL"
+print(values)
+del values [2]
+print(values)
