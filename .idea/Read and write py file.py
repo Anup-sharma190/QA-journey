95a68ab9698c099ac9f6669aa03b8e8e0e4a5278
Anup-sharma190 <anupcg06433@gmail.com>
2025-07-29 17:04:03 +0530

    all done perfect

---
+++ .idea/Read and write py file.py
@@ -0,0 +1,24 @@
+#file=open("test.txt")
+# read all the content of file
+#print(file.read())
+#file.close()
+# to close the file after read
+# if want to read only 2 charcter from file
+#file= open("test.txt")#read n number of character by passing parameter
+#print(file.read(5))# it will consider space also one character
+#file.close()
+# read line by line
+file=open("test.txt")
+#print(file.readline())# read one single line
+#print(file.readline())# if keep on writing these next next line will come to print
+#print(file.readline())
+#file.close()
+#print line by line using reading method
+file=open("test.txt")
+line= file.readline()
+while line != " ":
+    print(line)
+    line = file.readline()
+file.close()
+
+
