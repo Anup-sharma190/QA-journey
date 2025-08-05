bc59f66ed2ad68f9e2c858996e4436155a2fed2b
Anup-sharma190 <anupcg06433@gmail.com>
2025-08-05 12:38:21 +0530

    FINALLY DONE! PLEASE SUBMIT

---
+++ PROJECT UI CONTROL.PY.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.select import Select
+import time
+driver = webdriver.Chrome()  # ✅ Create Chrome driver object
+driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+checkboxes = driver.find_elements(By.XPATH,("//input[@type='checkbox']"))
+print(len(checkboxes))
+for checkbox in checkboxes:
+    if checkbox.get_attribute("value") == "option2":
+        checkbox.click()
+        assert checkbox.is_selected()
+        break
+print("all done maska")
+
