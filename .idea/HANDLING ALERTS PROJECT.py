b722cc273813dd774e70d36ffb1612396f719f1c
Anup-sharma190 <anupcg06433@gmail.com>
2025-08-07 13:08:53 +0530

    FINALLY DONE! PLEASE SUBMIT  IT.

---
+++ .idea/HANDLING ALERTS PROJECT.py
@@ -0,0 +1,20 @@
+from  selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium import webdriver
+#from selenium.webdriver.chrome.service import Service
+driver= webdriver.chrome()
+name = "rahul"
+driver = webdriver.Chrome()  # ✅ Create Chrome driver object
+driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+driver.find_element(By.CSS_SELECTOR,"#name").send_keys(name)
+driver.find_element(By.ID, "alertbtn").click()
+alert = driver.switch_to.alert
+alerttext= alert.text
+print(alerttext)
+assert name in alerttext
+alert.accept()#to select
+#alert.dismiss()# to cancell
+
+time.sleep(20)
+#alert.dismiss#to cancel on page
